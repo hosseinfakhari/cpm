ae946833b137771d3498543162ea5e903c21dad7
Hossein Fakhari <fakhari.hossein@gmail.com>
2023-08-25 13:06:33 +0100

    remove build directory from git

---
+++ src/lib/utils.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void walk() {
+    std::cout << "walking through all files and directories..." << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "lib/utils.h"
 
 int main() {
     std::cout << "Build tools and package manager for C/C++ source code" << std::endl;
+    walk();
     return 0;
 }
